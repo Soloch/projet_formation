1e8e122964c0610d3aed4a9b25df68fa5df1b30a
Rostazore <11044833+Rostazore@users.noreply.github.com>
2018-03-07 11:24:02 +0100

    Début traitement formulaire login

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = 3000;
+const session = require('express-session')
+const {body, check, validationResult} = require('express-validator/check')
+const {sanitizeBody} = require('express-validator/filter')
 app.set('views' , './views');
 app.set('view engine' , 'ejs');
 
@@ -12,15 +15,32 @@ app.get('/index', (req, res)=> {
 res.render('index.ejs', {title: "Bonjour"});
 });
 // Page de contact
-app.get('/contact' , (req , res) => {
-  res.render('contact.ejs' , {title: "Contact"});
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs', {title: "Contact"});
 });
- app.get ('/inscription' , (req , res)=> {
-   res.render('inscription.ejs' , {title: "Inscription"});
+ app.get ('/inscription', (req, res)=> {
+   res.render('inscription.ejs', {title: "Inscription"});
  });
 
-app.get('/login' , (req , res)=> {
-  res.render('login.ejs' , {title: "Connexion"});
+app.get('/login', (req, res)=> {
+  res.render('login.ejs', {title: "Connexion", erreurs: "Entrez votre email et votre mot de passe"});
+});
+
+/* Post pour le login. */
+app.post('/login', [
+  /* Vérification email. */
+  check('email').isEmail().withMessage('Doit être un email').trim().normalizeEmail(),
+  check('password').isLength({min: 5})
+], (req, res, next) => {
+  const erreurs = validationResult(req);
+  if (!erreurs.isEmpty())
+  {
+    res.render('login.ejs', {title: "Erreur", erreurs: erreurs.title});
+  }
+  else
+  {
+    res.render('login.ejs', {title: "Bon", erreurs: "Aucune erreur"});
+  }
 });
 
 /* Page d'administration. */
